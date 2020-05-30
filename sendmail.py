9b72a1f035a53fceaddf7571b0338a7b3c3fbe9c
pra-cloud <64471391+pra-cloud@users.noreply.github.com>
2020-05-30 15:04:14 +0530

    newmail code

--- sendmail.py
+++ sendmail.py
@@ -1,13 +1,50 @@
-import smtplib 
-# creates SMTP session 
-s = smtplib.SMTP('pm68199@gmail.com', 587) 
-# start TLS for security 
-s.starttls()   
-# Authentication 
-s.login("sender-mail", "password") 
-# message to be sent 
-message = "Hey Developer, Finally we got the model trained. "
-# sending the mail 
-s.sendmail("sender_mail", "developer_mail", message) 
-# terminating the session 
-s.quit()
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+email = 'Sender email'
+password = 'Sender email password'
+send_to_email = 'pm68199@gmail.com'
+subject = 'Model Accuracy'
+message = "Hey Developer!!! Check your final Model Accuracy after all tweaking. Now your model is giving desired accuracy."
+file_location = 'Output.txt'
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(message, 'plain'))
+
+# Setup the attachment
+filename = os.path.basename(file_location)
+attachment = open(file_location, "rb")
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+# Attach the attachment to the MIMEMultipart object
+msg.attach(part)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+text = msg.as_string()
+server.sendmail(email, send_to_email, text)
+server.quit()
+
+
+
+
+
+
