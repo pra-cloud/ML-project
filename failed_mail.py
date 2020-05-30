ec72f882e9bf680d0de899725db843869679edda
pra-cloud <64471391+pra-cloud@users.noreply.github.com>
2020-05-30 15:39:28 +0530

    prateek

--- failed_mail.py
+++ failed_mail.py
@@ -15,35 +15,12 @@ password = 'Sender email password'
 send_to_email = 'pm68199@gmail.com'
 subject = 'Model Accuracy'
 message = "Hey Developer!!! Check your final Model Accuracy after all tweaking. Now your model is giving desired accuracy."
-file_location = 'Output.txt'
+
 
 msg = MIMEMultipart()
 msg['From'] = email
 msg['To'] = send_to_email
 msg['Subject'] = subject
-
-msg.attach(MIMEText(message, 'plain'))
-
-# Setup the attachment
-filename = os.path.basename(file_location)
-attachment = open(file_location, "rb")
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-# Attach the attachment to the MIMEMultipart object
-msg.attach(part)
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(email, password)
-text = msg.as_string()
-server.sendmail(email, send_to_email, text)
-server.quit()
-
-
-
 
 
 
